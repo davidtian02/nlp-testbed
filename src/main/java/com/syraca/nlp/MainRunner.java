083a6ab93ea9ab57f270b690ee68b35eba7b51c4
David Tian <davidtian@utexas.edu>
2016-04-15 01:26:06 -0700

    Shuffling stuffs around

--- src/main/java/com/syraca/nlp/MainRunner.java
+++ src/main/java/com/syraca/nlp/MainRunner.java
@@ -1,10 +1,13 @@
-package com.syraca.nlp;
+package main.java.com.syraca.nlp;
 
 /**
  * Created by davidtian on 4/15/16.
  */
+import org.joda.time.LocalTime;
+
 public class MainRunner {
     public static void main(String... args) {
+        LocalTime currentTime = new LocalTime();
         System.out.println("hello nlp");
     }
 }
