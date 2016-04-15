145448a62c1891a9eda1023257f3404a8e7dda28
David Tian <davidtian@Davids-Mac-mini.local>
2016-04-15 00:50:30 -0700

    Initial commit

---
+++ src/com/syraca/nlp/MainRunner.java
@@ -0,0 +1,10 @@
+package com.syraca.nlp;
+
+/**
+ * Created by davidtian on 4/15/16.
+ */
+public class MainRunner {
+    public static void main(String... args) {
+        System.out.println("hello nlp");
+    }
+}
